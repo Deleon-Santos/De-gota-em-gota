c7400283c60b25be9aadad009d25134171a23909
Deleon <delps.santos1987@gmail.com>
2025-04-19 12:04:59 -0300

    alterações nas font, tamanho da font, tamanho da interface

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 
-
-
   const toggle = document.getElementById('menuToggle');
   const menu = document.getElementById('menuNavegacao');
 
   
   toggle.addEventListener('click', (e) => {
-    e.stopPropagation(); // Evita que o clique no botão feche o menu imediatamente
+    e.stopPropagation(); 
     menu.classList.toggle('show');
   });
 
@@ -24,27 +22,22 @@
     const botaoTopo = document.getElementById('button-topo');
   
     if (botaoTopo) {
-      // Função para verificar a posição da rolagem e atualizar a visibilidade do botão
       function verificarPosicaoScroll() {
         if (window.scrollY === 0) {
           botaoTopo.style.display = 'none';
         } else {
-          botaoTopo.style.display = 'block'; // Ou 'flex' ou o display que você estiver usando
+          botaoTopo.style.display = 'block'; 
         }
       }
   
-      // Chama a função inicialmente para verificar se já estamos no topo
       verificarPosicaoScroll();
   
-      // Adiciona um listener para o evento de scroll da janela
       window.addEventListener('scroll', verificarPosicaoScroll);
   
-      // Adiciona o listener para o clique no botão para voltar ao topo
       botaoTopo.addEventListener('click', function(event) {
         event.preventDefault();
         window.scrollTo({
-          top: 0,
-          behavior: 'smooth'
+          top: 0,         
         });
       });
     }
